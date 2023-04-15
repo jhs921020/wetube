37ced80e283b48ea0fdab185270932d315defcac
jhs921020 <jhs921020@nate.com>
2023-04-15 12:38:19 +0900

    pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,37 @@
 // videoController
-export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const search = (req, res) => res.send("Search Video");
+export const upload = (req, res) => res.send("Upload Video");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
