99682dbbdf8ab9c5bf359e585d8b729de097874d
jhs921020 <jhs921020@nate.com>
2023-04-27 18:15:36 +0900

    7.15 Environment Variables

--- src/server.js
+++ src/server.js
@@ -19,12 +19,14 @@ app.use(
   })
 );
 
+console.log(process.env.COOKIE_SECRET);
+
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
