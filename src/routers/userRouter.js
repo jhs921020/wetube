c56a1a10f8af09709ece5e1f926c19c9c57bc8eb
jhs921020 <jhs921020@nate.com>
2023-04-30 10:04:30 +0900

    8.0 Edit Profile GET

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,17 +1,18 @@
 // userRouter
 import express from "express";
 import {
-  edit,
   logout,
   see,
   startGithubLogin,
   finishGithubLogin,
+  getEdit,
+  postEdit,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
