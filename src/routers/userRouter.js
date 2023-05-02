3304d90b8e2cd3ca49a6e3321b4027739ae6cd5e
jhs921020 <jhs921020@nate.com>
2023-05-02 22:08:17 +0900

    8.6 File Uploads part One

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,6 @@
 // userRouter
 import express from "express";
+import multer from "multer";
 import {
   logout,
   see,
@@ -10,12 +11,20 @@ import {
   getChangePassword,
   postChangePassword,
 } from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
+import {
+  protectorMiddleware,
+  publicOnlyMiddleware,
+  uploadFiles,
+} from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(uploadFiles.single("avatar"), postEdit);
 userRouter
   .route("/change-password")
   .all(protectorMiddleware)
